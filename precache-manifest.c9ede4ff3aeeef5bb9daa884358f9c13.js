4a3f5e0223fb0372aea5c58a4ca87a84e8c6b57f
NTPhuong <thaiphuongn1@gmail.com>
2020-03-29 16:37:56 +0700

    Updates

---
+++ precache-manifest.c9ede4ff3aeeef5bb9daa884358f9c13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79eb2b41c8c17ff9060950b80b78de81",
+    "url": "/Lease-On-The-Block/index.html"
+  },
+  {
+    "revision": "6acbbbeab1feabf7d96c",
+    "url": "/Lease-On-The-Block/static/css/main.a9d0e2b9.chunk.css"
+  },
+  {
+    "revision": "ba8e85a8e7513d516991",
+    "url": "/Lease-On-The-Block/static/js/2.8eb5fc1d.chunk.js"
+  },
+  {
+    "revision": "6acbbbeab1feabf7d96c",
+    "url": "/Lease-On-The-Block/static/js/main.9be454e9.chunk.js"
+  },
+  {
+    "revision": "2ffa999d220bacf1cd17",
+    "url": "/Lease-On-The-Block/static/js/runtime-main.8c53d488.js"
+  }
+]);
